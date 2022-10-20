3c07ac212fac96192dfb6364d8fd873e4be269d9
addardey <agaodei34@gmail.com>
2022-10-20 09:59:15 -0700

    more C

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,12 +1,11 @@
 #include "main.h"
+
 /**
- * add - Adds two integers as num1 and num2
+ * add - Adds two integers as num1 and num2.
+ * @num1: The first integer to be added is num1.
+ * @num2: The second integer to be added is num2.
  *
- * @num1: The first integers to be added is num1
- *
- * @num2: The second number to be added is num2
- *
- * Return: the results of the addition
+ * Return: The result of the addition.
  */
 int add(int num1, int num2)
 {
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,7 +1,7 @@
 #include "main.h"
+
 /**
  * print_last_digit - Prints the last digit of a number.
- *
  * @n: The number in question.
  *
  * Return: The Value of the last digit.
@@ -12,6 +12,8 @@ int print_last_digit(int n)
 
 	if (last_digit < 0)
 		last_digit *= -1;
+
 	_putchar(last_digit + '0');
+
 	return (last_digit);
 }
