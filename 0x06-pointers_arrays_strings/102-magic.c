8b9f9478057b77b5bf2dcb93301f17a2c9576a5f
addardey <agaodei34@gmail.com>
2022-10-28 09:09:43 -0700

    try

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -8,9 +8,11 @@ int main(void)
 
 	a[2] = 1024;
 	p = &n;
-	/*
+	/**
+	* main: magic
 	* write your line of code here...
 	* Remember:
+	* main - magic
 	* - you are not allowed to use a
 	* - you are not allowed to modify p
 	* - only one statement
